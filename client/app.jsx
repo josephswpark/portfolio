a91742a3bc9e1a15412729687b1199ad6a048ac3
Joseph Park <parkjoseph54@gmail.com>
2023-02-07 06:25:06 +0000

    done!

--- client/app.jsx
+++ client/app.jsx
@@ -4,6 +4,10 @@ import AboutMe from './pages/about-me';
 import Technologies from './pages/technologies';
 import { ActiveItem } from 'react-scroll-slider';
 import Projects from './pages/projects';
+import Contact from './pages/contact';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 export default class App extends React.Component {
   render() {
@@ -20,8 +24,33 @@ export default class App extends React.Component {
           <div id='technologies' className='technologies'>
             <Technologies />
           </div>
+          <div id='contact' className='contact'>
+            <Contact />
+          </div>
+          <Box component="footer"
+            sx={{
+              py: 3,
+              px: 2,
+              mt: 'auto'
+            }}
+          >
+            <Container maxWidth="sm" style={{ textAlign: 'center' }}>
+              <Copyright />
+            </Container>
+          </Box>
         </ActiveItem>
       </>
     );
   }
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {'Handcrafed by Joseph Park © '}
+      {' '}
+      {new Date().getFullYear()}
+      .
+    </Typography>
+  );
 }
