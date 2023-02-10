7cbf370358a2582eb686c5ce1ad6e710983486bb
Joseph Park <parkjoseph54@gmail.com>
2023-02-10 00:38:49 +0000

    hello2

--- database/import.sh
+++ database/import.sh
@@ -1,17 +1,17 @@
-#!/bin/sh
+!/bin/sh
 
-# set -e
+set -e
 
-# if [ -f "$PWD"/.env ]; then
-# # shellcheck source=/dev/null
-#   . "$PWD"/.env
-# fi
+if [ -f "$PWD"/.env ]; then
+# shellcheck source=/dev/null
+  . "$PWD"/.env
+fi
 
-# if [ -n "$DATABASE_URL" ]; then
-#   psql "$DATABASE_URL" \
-#     -f "$PWD"/database/schema.sql \
-#     -f "$PWD"/database/data.sql
-# else
-#   echo 'no DATABASE_URL environment variable set' 1>&2
-#   exit 1
-# fi
+if [ -n "$DATABASE_URL" ]; then
+  psql "$DATABASE_URL" \
+    -f "$PWD"/database/schema.sql \
+    -f "$PWD"/database/data.sql
+else
+  echo 'no DATABASE_URL environment variable set' 1>&2
+  exit 1
+fi
